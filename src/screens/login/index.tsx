834b935c2a5a3de6fa3d115cad949f242c3b71bd
mac <gos360@163.com>
2022-05-17 23:50:50 +0800

    feat: 使用控制台工具插件mock后端api

--- src/screens/login/index.tsx
+++ src/screens/login/index.tsx
@@ -1,22 +1,38 @@
 import {FormEvent} from "react";
 
 export const LoginScreen = () => {
-    const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
-        event.preventDefault()
-        const username = (event.currentTarget[0] as HTMLInputElement).value
-        const password = (event.currentTarget[1] as HTMLInputElement).value
-        console.log(username, password)
+    const handleLogin = (form: { username: string, password: string }) => {
+        fetch('http://localhost:3001/login', {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify(form)
+        }).then(
+            res => {
+                if (res.ok) {
+                    return res.json()
+                }
+                return Promise.reject(form)
+            }
+        )
     }
-    return (<form onSubmit={handleSubmit}>
-        <div>
-            <label htmlFor={"username"}>用户名</label>
-            <input type="text"/>
-        </div>
-        <div>
-            <label htmlFor={"password"}>密码</label>
-            <input type="password"/>
-        </div>
-        <button type={"submit"}>登录</button>
-    </form>)
+
+const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+    const username = (event.currentTarget[0] as HTMLInputElement).value
+    const password = (event.currentTarget[1] as HTMLInputElement).value
+    console.log(username, password)
+    handleLogin({username, password})
+}
+return (<form onSubmit={handleSubmit}>
+    <div>
+        <label htmlFor={"username"}>用户名</label>
+        <input type="text"/>
+    </div>
+    <div>
+        <label htmlFor={"password"}>密码</label>
+        <input type="password"/>
+    </div>
+    <button type={"submit"}>登录</button>
+</form>)
 
 }
